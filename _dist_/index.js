50c8034542920f0a3013c6acefc9c5daf0de16c8
mh3mh0 <mh3mh0@users.noreply.github.com>
2021-06-23 06:19:24 +0000

    deploy: d77f59232ae0450e094a9c2cba5e6e1cf302d42a

---
+++ _dist_/index.js
@@ -0,0 +1,66 @@
+import card from "./components/card.js";
+
+const cardsInserted = [];
+
+const cardsContainer = document.querySelector("div.cards-container");
+
+const closeButton = (event) => {
+  if (
+    event.target.nodeName === "svg" ||
+    event.target.parentNode.nodeName === "svg"
+  ) {
+    if (event.target.parentElement.nodeName === "DIV") {
+      const elId = parseInt(event.target.parentElement.dataset.id);
+      const index = cardsInserted.indexOf(elId);
+      cardsInserted.splice(index, 1);
+      event.target.parentElement.remove();
+    } else {
+      const elId = parseInt(
+        event.target.parentElement.parentElement.dataset.id
+      );
+      const index = cardsInserted.indexOf(elId);
+      cardsInserted.splice(index, 1);
+      event.target.parentElement.parentElement.remove();
+    }
+  }
+};
+
+cardsContainer.addEventListener("click", closeButton);
+
+const renderData = () => {
+  const input = document.querySelector("input");
+  const submitButton = document.querySelector("button");
+
+  submitButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    if (!input.value) {
+      input.classList.add("bad-input");
+      input.placeholder = "Add a location";
+      return;
+    }
+
+    // @ts-ignore
+    const { cardContainer, id } = await card(input.value);
+    if (!cardContainer) {
+      input.value = "";
+      input.classList.add("bad-input");
+      input.placeholder = "Invalid location";
+      return;
+    }
+
+    if (cardsInserted.includes(id)) {
+      input.classList.add("bad-input");
+      input.placeholder = "Already searched";
+      input.value = "";
+      return;
+    }
+
+    input.classList.remove("bad-input");
+    cardsContainer.insertAdjacentElement("afterbegin", cardContainer);
+    input.value = "";
+    input.placeholder = "Add a location";
+    cardsInserted.push(id);
+  });
+};
+
+renderData();
---
+++ _dist_/utilities/convertTemperature.js
@@ -0,0 +1,7 @@
+const kelvinToCelsius = (tempInKelvin) => {
+  const kelvin = 273.15;
+  const tempInCelsius = Math.floor(tempInKelvin - kelvin);
+  return tempInCelsius;
+};
+
+export default kelvinToCelsius;
